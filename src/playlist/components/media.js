30d6dd620d2a30488a8e609b60f3b896891af766
Eduardo Denis <edigde@gmail.com>
2019-01-13 15:14:57 -0300

    fix styles

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -17,8 +17,6 @@ class Media extends Component {
             className="Media-image "
             src="./../../../images/covers/bitcoin.jpg"
             alt=""
-            width={260}
-            height={160}
           />
           <h3 className="Media-title">¿Por qué aprender React?...</h3>
           <p  className="Media-author">Eduardo Denis</p>
