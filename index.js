43fb55298167b94406b98f2204faa627c3771b86
Eduardo Denis <edigde@gmail.com>
2019-01-13 13:20:28 -0300

    react-dom render

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+
 console.log('Hola mundo!')
+const app = document.getElementById('app')
+const template = <h1>Hola Mundo!</h1>
+ReactDom.render(template, app)
+
