16dfa0709c9874e318f0e3bdf14cc4fa8c265aaf
Eduardo Denis <edigde@gmail.com>
2019-01-13 15:01:15 -0300

    agregando styles al componente (by inline style and by class)

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import './media.css'
 
 class Media extends Component {
   render() {
+    const styles = {
+      container: {
+        border: '1px solid black',
+        boxShadow: '0 0 10px rgba(0,0,0,.3)'
+      }
+    }
+
     return (
-      <div>
-        <div>
+      <div className="Media" style={styles.container}>
+        <div className="Media-cover">
           <img
-            src=""
+            className="Media-image "
+            src="./../../../images/covers/bitcoin.jpg"
             alt=""
             width={260}
             height={160}
           />
-          <h3>¿Por qué aprender React?...</h3>
-          <p>Eduardo Denis</p>
+          <h3 className="Media-title">¿Por qué aprender React?...</h3>
+          <p  className="Media-author">Eduardo Denis</p>
         </div>
       </div>
     )
