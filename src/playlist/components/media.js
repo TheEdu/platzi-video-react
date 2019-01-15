78b4f6b556fc4c2d610c47915ca391e2ec54d33b
TheEdu <edigde@gmail.com>
2019-01-15 13:48:12 -0300

    add docs

--- src/playlist/components/media.js
+++ src/playlist/components/media.js
@@ -2,14 +2,6 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import './media.css'
 
-/*
-  PureComponent: tiene el método shouldComponentUpdate ya asignado (por defecto),
-  si a este componente no se le actualizan las propiedades,
-  no tenemos que validar a mano con shouldComponentUpdate,
-  PureComponent lo hace por nosotros, es decir;
-  si recibe nuevas propiedades pero son las que ya teniamos, no se re-renderiza.
-*/
-
 class Media extends PureComponent {
 
   // constructor(props){
--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -7,27 +7,6 @@ import Pause from './../../icons/components/pause'
 import Volume from './../../icons/components/volume'
 import FullScreen from './../../icons/components/full-screen'
 
-
-/*
-  Componente Funcional: Es una función la cual solo retorna el JSX de nuestro componente (renderiza UI),
-  es mas sencillo, mas fácil de probar y este componente no tiene ciclo de vida.
-*/
-
-// // Componente Funcional
-// function Playlist(props){
-//     const playlist = props.data.categories[0].playlist
-//     console.log(props.data);
-//     return (
-//       <div className="Playlist">
-//         {
-//           playlist.map((item) => {
-//             return <Media {...item} key={item.id} />
-//           })
-//         }
-//       </div>
-//     )
-// }
-
 class Playlist extends Component {
 
   render() {
