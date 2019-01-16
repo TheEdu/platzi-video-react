1f6ac2493ef6f551476ff75117101b8f15441cd2
Eduardo Denis <edigde@gmail.com>
2019-01-16 00:53:55 -0300

    add related UI-Component to home-layout childrens + css

--- src/pages/components/home-layout.js
+++ src/pages/components/home-layout.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './home-layout.css'
 
 function HomeLayout(props) {
     return (
-        <section>
-            <h1> HomeLayout </h1>
+        <section className="HomeLayout">
+            {/* <h1> HomeLayout </h1> */}
             {/* Render all childrens */}
             { props.children }
         </section>
--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import HomeLayout from './../components/home-layout';
 import Categories from './../../categories/components/categories';
+import Related from './../components/related';
 
 class Home extends Component {
     render() {
         const categories = this.props.data.categories
-        console.log(categories)
         return (
             <HomeLayout>
-                HomeLayout Childrens
+                <Related />
                 <Categories categories={ categories } />
             </ HomeLayout>
         )
