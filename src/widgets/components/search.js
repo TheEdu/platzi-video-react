4998cceecac7b24c6e3ca37bd398868ba4405ce1
TheEdu <edigde@gmail.com>
2019-01-17 12:27:37 -0300

    Referencias a Eventos de HTML y Formularios

--- src/widgets/components/search.js
+++ src/widgets/components/search.js
@@ -2,11 +2,18 @@ import React from 'react'
 import './search.css'
 
 const Search = (props) => (
-  <form className="Search">
-    <input 
+  <form 
+    className="Search"
+    onSubmit = { props.handleSubmit }
+  >
+    <input
+      ref = { props.setInputRef }
+      value = { props.value }
+      onChange = { props.handleChange }
       type="text" 
       className="Search-input"
       placeholder="Search"
+      // defaultValue="Luis Fonsi"
     />
   </form>
 )
--- src/widgets/containers/search.js
+++ src/widgets/containers/search.js
@@ -2,9 +2,37 @@ import React, { Component } from 'react'
 import Search from './../components/search'
 
 class SearchContainer extends Component {
+
+  state = {
+    value: "Luis-Fonsi"
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    console.log("submit", this.input.value)
+  }
+
+  handleInputChange= event => {
+    const newValue = event.target.value
+    console.log(newValue)
+    this.setState({
+      value: newValue.replace(" ", "-")
+    })
+  }
+
+  setInputRef = element => {
+    this.input = element
+  }
+
   render() {
     return (
-      <Search />
+      <Search
+        setInputRef  = { this.setInputRef  }
+        value  = { this.state.value }
+        handleChange = { this.handleInputChange }
+
+        handleSubmit = { this.handleSubmit }
+      />
     )
   }
 }
