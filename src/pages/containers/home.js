3730fdcbf27c74125eac5d681783e6842b4ea4d2
TheEdu <edigde@gmail.com>
2019-01-17 08:04:50 -0300

    Add modal UI Component + close button and handleModalToggel

--- src/pages/containers/home.js
+++ src/pages/containers/home.js
@@ -3,17 +3,36 @@ import HomeLayout from './../components/home-layout';
 import Categories from './../../categories/components/categories';
 import Related from './../components/related';
 import ModalContainer from './../../widgets/containers/modal';
+import Modal from './../../widgets/components/modal';
 
 class Home extends Component {
+    state = {
+        modalDisplay: false
+    }
+
+    handleModalToggle = (event) => {
+        this.setState({
+            modalDisplay: !this.state.modalDisplay,
+        })
+    }
+
     render() {
         const categories = this.props.data.categories
         return (
             <HomeLayout>
                 <Related />
                 <Categories categories={ categories } />
-                <ModalContainer>
-                    <div> Esto es un Portal </div>
-                </ModalContainer>
+                {
+                    // If ternario
+                    this.state.modalDisplay &&
+                    <ModalContainer>
+                        <Modal 
+                            handleClick={ this.handleModalToggle }
+                        >
+                            Esto es un Modal (en un Portal)
+                        </Modal>
+                    </ModalContainer>
+                }
             </HomeLayout>
         )
     }
