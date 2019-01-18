2c2fdc85dcf7eaedfe915775b125f2b5307d3272
TheEdu <edigde@gmail.com>
2019-01-18 10:36:06 -0300

    Play / Pausa: Creando la UI

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,13 +2,29 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from './../components/video-player-layout'
 import Video from './../components/video'
 import Title from './../components/title'
+import PlayPause from './../components/play-pause'
 
 class VideoPlayer extends Component {
+
+  state = {
+    play: false,
+  }
+
+  togglePlay = (event) => {
+    this.setState({
+      play: !this.state.play
+    })
+  }
+
   render() {
     return (
       <VideoPlayerLayout>
         <Title 
           title="Esto es un Video Chido!"
+        />
+        <PlayPause
+          play={this.state.play}
+          handleClick={this.togglePlay}
         />
         <Video
           muted={true}
